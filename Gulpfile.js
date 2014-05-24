f57e8379d640198a7a28c177f1868308294fea8c
Robert Juliano <rovjuvano@gmail.com>
2014-05-24 18:17:21 -0400

    refactor file watching

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,27 @@
 var gulp = require('gulp'),
 	clean = require('gulp-clean'),
+	cached = require('gulp-cached'),
 	jadeToHtml = require('gulp-jade'),
 	lessToCss = require('gulp-less'),
 	cssAutoprefixer = require('gulp-autoprefixer'),
 	mocha = require('gulp-mocha'),
 	pkg = require('./package.json');
 
+const DIST_DIR = 'dist';
+
 var paths = {
-	jade: ['./src/**/*.jade'],
-	less: ['./src/**/*.less'],
-	raw: ['./src/**/*.js',
+	jade: ['src/**/*.jade'],
+	less: ['src/**/*.less'],
+	raw: ['src/**/*.js',
 		'bower_components/angular/angular.min.js{,.map}',
 	],
-	test: 'spec/**/*{[-_]s,S}pec.js',
+	test: {
+		run: 'spec/**/*{[-_]s,S}pec.js',
+		watch: ['spec/**/*', DIST_DIR + '**/*'],
+	},
 }
 
-const DIST_DIR = './dist';
-
-gulp.task('default', ['test', 'watch']);
+gulp.task('default', ['watch']);
 gulp.task('clean', function() {
 	gulp.src(DIST_DIR, {read: false})
 		.pipe(clean());
@@ -26,33 +30,32 @@ gulp.task('clean', function() {
 gulp.task('build', ['build:jade', 'build:less', 'build:raw']);
 gulp.task('build:jade', function() {
 	return gulp.src(paths.jade)
+		.pipe(cached('src'))
 		.pipe(jadeToHtml())
 		.pipe(gulp.dest(DIST_DIR));
 });
 gulp.task('build:less', function() {
 	return gulp.src(paths.less)
+		.pipe(cached('src'))
 		.pipe(lessToCss())
 		.pipe(cssAutoprefixer())
 		.pipe(gulp.dest(DIST_DIR));
 });
 gulp.task('build:raw', function() {
 	return gulp.src(paths.raw)
+		.pipe(cached('src'))
 		.pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.jade, ['watch:jade']);
-	gulp.watch(paths.less, ['watch:less']);
-	gulp.watch(paths.raw, ['watch:raw']);
-	gulp.watch('spec/**/*', ['watch:test']);
+gulp.task('watch', ['test'], function() {
+	gulp.watch(paths.jade, ['build:jade']);
+	gulp.watch(paths.less, ['build:less']);
+	gulp.watch(paths.raw, ['build:raw']);
+	gulp.watch(paths.test.watch, ['test:unit']);
 });
-gulp.task('watch:jade', ['build:jade'], runTests);
-gulp.task('watch:less', ['build:less']);
-gulp.task('watch:raw', ['build:raw'], runTests);
-gulp.task('watch:test', [], runTests);
 
-gulp.task('test', ['build'], runTests);
-function runTests() {
-   return gulp.src(paths.test, { read: false })
-        .pipe(mocha({ reporter: 'min' }));
-};
+gulp.task('test', ['build'], function() { return gulp.start('test:unit') });
+gulp.task('test:unit', function() {
+	return gulp.src(paths.test.run, { read: false })
+		.pipe(mocha({ reporter: 'min' }));
+});
